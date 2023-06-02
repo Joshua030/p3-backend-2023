19dbca12d8edccd9890875c55bf9313e5aaa392a
Joshua030 <Joseluy123>
2023-06-02 02:27:21 +0200

    first Route get

--- backpractice/src/server.ts
+++ backpractice/src/server.ts
@@ -22,65 +22,68 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-
-app.get('/', async (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   try {
-    const { userId } = req.body;
-    // Fetch data from NASA API
-    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1');
+    const { userId } = req.query;
+
+    const response = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1"
+    );
     const data = await response.json();
 
     if (data.length > 0) {
-      const post:Post = data[0];
+      const post: Post = data[0];
 
-      // Check if PostDetails already exists
       const existingPostDetails = await prisma.postDetails.findUnique({
-        where: { postDate: post.date },
+        where: { postId: post.date },
       });
 
-      if (!existingPostDetails) {
-        // Create PostDetails if it doesn't exist
-        const newPostDetails = await prisma.postDetails.create({
-          data: {
-            postDate: post.date,
-            explanation: post.explanation,
-            hdurl: post.hdurl,
-            media_type: post.media_type,
-            service_version: post.service_version,
-            postId: null, // This will be updated later
-          },
-        });
+ 
 
-        // Associate PostDetails with a Post
-        await prisma.post.update({
-          where: { id: postId },
-          data: { postDetails: { connect: { id: newPostDetails.id } } },
+      if (!existingPostDetails) {
+      
+        const result = await prisma.post.create({
+          data: {
+            date: post.date,
+            title: post.title,
+            url: post.url,
+            watched: true,
+            userId: Number(userId),
+            postDetails: {
+              create: {
+                explanation: post.explanation,
+                hdurl: post.hdurl,
+                media_type: post.media_type,
+                service_version: post.service_version,
+              },
+            },
+          },
+          include: {
+            postDetails: true, // Include the post in the returned object
+          },
         });
       }
 
-      // Check if the user has watched the Post
-       // Assuming you have user authentication implemented
-      const watchedPost = await prisma.post.findFirst({
-        where: { id: postId, userId, watched: true },
-      });
+            if (existingPostDetails) {
+              const watchedPost = await prisma.post.findFirst({
+                where: { date: post.date, userId: Number(userId), watched: true },
+              });
 
-      if (!watchedPost) {
-        // Update watched status if the user hasn't watched the Post
-        await prisma.post.update({
-          where: { id: postId },
-          data: { watched: true },
-        });
+              if (!watchedPost) {
+                await prisma.post.update({
+                  where: { date: post.date },
+                  data: { watched: true },
+                });
+              }
       }
     }
 
-    res.send('Data fetched and updated successfully.');
+    res.send("Data fetched and updated successfully.");
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('An error occurred.');
+    console.error("Error:", error);
+    res.status(500).send("An error occurred.");
   }
 });
-
-
 
 const { SERVER_PORT } = process.env;
 app.listen(SERVER_PORT, () => {
